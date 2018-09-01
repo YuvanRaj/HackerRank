6c51cf1a55e1ee7a6009e2710730ff600d781497
YuvanRaj <yuviram91@gmail.com>
2018-09-02 00:30:29 +0530

    Correction on package name

--- HackerRankSolution/src/main/java/com/hackerrank/algorithms/strings/SherlockAndValidString.java
+++ HackerRankSolution/src/main/java/com/hackerrank/algorithms/strings/SherlockAndValidString.java
@@ -1,10 +1,5 @@
-package com.hackerrank.algorithams.string;
+package com.hackerrank.algorithms.strings;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
